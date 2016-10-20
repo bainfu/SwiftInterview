a92bc660c082d72514dd81b70f251bcbf8e4b0e6
Wesley St. John <wesley.stjohn@mobileforming.com>
2016-10-20 16:13:40 -0700

    First Commit

---
+++ SwiftInterview/SwiftInterview/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftInterview
+//
+//  Created by Wesley St. John on 10/20/16.
+//  Copyright © 2016 mobileforming. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
