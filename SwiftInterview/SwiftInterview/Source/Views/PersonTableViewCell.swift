8a18ad5645b476b7b9adebe45484d86a90257132
Wesley St. John <wesley.stjohn@mobileforming.com>
2016-10-20 17:31:55 -0700

    Added table view cell class

---
+++ SwiftInterview/SwiftInterview/Source/Views/PersonTableViewCell.swift
@@ -0,0 +1,46 @@
+//
+//  PersonTableViewCell.swift
+//  SwiftInterview
+//
+//  Created by Wesley St. John on 10/20/16.
+//  Copyright © 2016 mobileforming. All rights reserved.
+//
+
+import UIKit
+
+class PersonTableViewCell: UITableViewCell {
+
+    var person: Person? {
+        didSet {
+            nameLabel?.text = person?.name
+            descriptionLabel?.text = person?.personDescription
+            
+            if let urlString = person?.imageURL,
+                let url = URL(string: urlString),
+                let data = try? Data(contentsOf: url) {
+                
+                profileImageView?.image = UIImage(data: data)
+            } else {
+                profileImageView?.image = nil
+            }
+        }
+    }
+    
+    
+    @IBOutlet var profileImageView: UIImageView?
+    @IBOutlet var nameLabel: UILabel?
+    @IBOutlet var descriptionLabel: UILabel?
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
