2b2dcabae0b5616247a9127e1b8df7b8f20243fb
Wesley St. John <wesley.stjohn@mobileforming.com>
2016-10-20 17:57:47 -0700

    Starting to implement tableview datasource

--- SwiftInterview/SwiftInterview/Source/ViewControllers/ViewController.swift
+++ SwiftInterview/SwiftInterview/Source/ViewControllers/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // TODO: move this somewhere else
+    var people: [Person]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +24,17 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+extension ViewController: UITableViewDataSource {
+ 
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return (people?.count)!
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        return UITableViewCell()
+    }
 }
 
